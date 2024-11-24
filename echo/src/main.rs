f4e503db25b9f7969ed55c77dd7a631e9b16a43a
volodymyrd <volodymyrdotsenko@gmail.com>
2024-11-24 21:14:31 +0100

    Parsing a stream of bytes as lines.

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,24 +1,19 @@
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener};
+use futures::{SinkExt, StreamExt};
+use tokio::net::TcpListener;
+use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let server = TcpListener::bind("127.0.0.1:42069").await?;
     loop {
         let (mut tcp, _) = server.accept().await?;
-        let mut buffer = [0u8; 16];
-        loop {
-            let n = tcp.read(&mut buffer).await?;
-            if n == 0 {
-                break;
-            }
-            // convert byte slice to a String
-            let mut line = String::from_utf8(buffer[..n].to_vec())?;
-            // remove line terminating chars added by telnet
-            line.pop(); // remove \n char
-            line.pop(); // remove \r char
-            // add our own line terminator :)
-            line.push_str(" ❤️\n");
-            let _ = tcp.write(line.as_bytes()).await?;
+        let (reader, writer) = tcp.split();
+        let mut stream = FramedRead::new(reader, LinesCodec::new());
+        let mut sink = FramedWrite::new(writer, LinesCodec::new());
+
+        while let Some(Ok(mut msg)) = stream.next().await {
+            msg.push_str(" ❤️");
+            sink.send(msg).await?;
         }
     }
 }
