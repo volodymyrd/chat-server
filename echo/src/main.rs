81837d5a912ef6b55a966747c5f4c66c0e8cc39a
volodymyrd <volodymyrdotsenko@gmail.com>
2024-12-01 23:00:39 +0100

    add help and quit commands.

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,6 +1,8 @@
-use futures::{StreamExt};
+use futures::{SinkExt, StreamExt};
 use tokio::net::TcpListener;
 use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
+
+const HELP_MSG: &str = include_str!("help.txt");
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -8,13 +10,22 @@ async fn main() -> anyhow::Result<()> {
     loop {
         let (mut tcp, _) = server.accept().await?;
         let (reader, writer) = tcp.split();
-        let stream = FramedRead::new(reader, LinesCodec::new());
-        let sink = FramedWrite::new(writer, LinesCodec::new());
+        let mut stream = FramedRead::new(reader, LinesCodec::new());
+        let mut sink = FramedWrite::new(writer, LinesCodec::new());
 
-        stream.map(|msg| {
-            let mut msg = msg?;
-            msg.push_str(" ❤️");
-            Ok(msg)
-        }).forward(sink).await?
+        // send list of server commands to
+        // the user as soon as they connect
+        sink.send(HELP_MSG).await?;
+
+        while let Some(Ok(mut msg)) = stream.next().await {
+            if msg.starts_with("/help") { // handle new /help command
+                sink.send(HELP_MSG).await?;
+            } else if msg.starts_with("/quit") { // handle new /quit command
+                break;
+            } else {
+                msg.push_str(" ❤️");
+                sink.send(msg).await?;
+            }
+        }
     }
 }
