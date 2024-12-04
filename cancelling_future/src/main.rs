85d7b1df2767f1d84b361ec747f3795072836863
volodymyrd <volodymyrdotsenko@gmail.com>
2024-12-04 23:02:37 +0100

    improve future experiment.

--- cancelling_future/src/main.rs
+++ cancelling_future/src/main.rs
@@ -11,24 +11,18 @@ async fn count_to(num: u8) {
 #[tokio::main]
 async fn main() {
     println!("start counting");
-    // the select! macro polls each
-    // future until one of them completes,
-    // and then we execute the match arm
-    // of the completed future and drop
-    // all of the other futures
+    let count_to_10 = count_to(10);
+    tokio::pin!(count_to_10);
     tokio::select! {
         _ = count_to(3) => {
             println!("counted to 3");
         },
-        _ = count_to(10) => {
+        _ = &mut count_to_10 => {
             println!("counted to 10");
         },
     };
     println!("stop counting");
-    // this sleep is here to demonstrate
-    // that the count_to(10) doesn't make
-    // any progress after we stop polling
-    // it, even if we go to sleep and do
-    // nothing else for a while
-    sleep(Duration::from_millis(1000)).await;
+    println!("jk, keep counting");
+    count_to_10.await;
+    println!("finished counting to 10");
 }
